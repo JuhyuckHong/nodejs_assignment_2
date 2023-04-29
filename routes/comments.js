a6dcf0a26cd38f297f291c6d6ee050181e888018
Juhyuck-Hong <juhyukhong@gmail.com>
2023-04-29 18:44:30 +0900

    Add authentication middleware to posts and comments routes

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const { Comments } = require("../models")
+const auth = require("../middlewares/auth-middleware")
 const router = express.Router()
 
 // 6. 댓글 작성 API
 //     - 로그인 토큰을 검사하여, 유효한 토큰일 경우에만 댓글 작성 가능
 //     - 댓글 내용을 비워둔 채 댓글 작성 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
 //     - 댓글 내용을 입력하고 댓글 작성 API를 호출한 경우 작성한 댓글을 추가하기
-router.post("/posts/:postId/comments", (req, res) => {
+router.post("/posts/:postId/comments", auth, (req, res) => {
 
 })
 
@@ -14,15 +15,15 @@ router.post("/posts/:postId/comments", (req, res) => {
 //     - 로그인 토큰을 검사하여, 해당 사용자가 작성한 댓글만 수정 가능
 //     - 댓글 내용을 비워둔 채 댓글 수정 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
 //     - 댓글 내용을 입력하고 댓글 수정 API를 호출한 경우 작성한 댓글을 수정하기
-router.put("/posts/:postId/comments/:commentId", (req, res) => {
-    
+router.put("/posts/:postId/comments/:commentId", auth, (req, res) => {
+
 })
 
 // 8. 댓글 삭제 API
 //     - 로그인 토큰을 검사하여, 해당 사용자가 작성한 댓글만 삭제 가능
 //     - 원하는 댓글을 삭제하기
-router.delete("/posts/:postId/comments/:commentId", (req, res) => {
-    
+router.delete("/posts/:postId/comments/:commentId", auth, (req, res) => {
+
 })
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { Posts } = require("../models")
+const auth = require("../middlewares/auth-middleware")
 const router = express.Router()
 
 // 1. 전체 게시글 목록 조회 API
@@ -12,7 +13,7 @@ router.get("/posts", (req, res) => {
 // 2. 게시글 작성 API
 //     - 토큰을 검사하여, 유효한 토큰일 경우에만 게시글 작성 가능
 //     - 제목, 작성 내용을 입력하기
-router.post("/posts", (req, res) => {
+router.post("/posts", auth, (req, res) => {
 
 })
 
@@ -25,13 +26,13 @@ router.get("/posts/:postId", (req, res) => {
 
 // 4. 게시글 수정 API
 //     - 토큰을 검사하여, 해당 사용자가 작성한 게시글만 수정 가능
-router.put("/posts/:postId", (req, res) => {
+router.put("/posts/:postId", auth, (req, res) => {
 
 })
 
 // 5. 게시글 삭제 API
 //     - 토큰을 검사하여, 해당 사용자가 작성한 게시글만 삭제 가능
-router.delete("/posts/:postId", (req, res) => {
+router.delete("/posts/:postId", auth, (req, res) => {
 
 })
 
