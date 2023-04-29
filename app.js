084ab6e9c8cb215779086a23110a20c1bb8f9de9
Juhyuck-Hong <juhyukhong@gmail.com>
2023-04-29 13:56:55 +0900

    Initialize app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const app = express()
+const PORT = 3018
+
+// cookie-parser 사용
+app.use(cookieParser())
+
+// request json parsing, route 사용
+app.use(express.json())
+app.use('/api', [])
+
+// 랜딩페이지 눈부심 방지
+app.get('/', (req, res) => {
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+        <title>My Page</title>
+    </head>
+    <body style="background-color: black; color: white;">
+        <h1>Node 숙련주차 과제</h1>
+        <small> 홍주혁 파트타임1기</small>
+    </body>
+    </html>`)
+});
+
+// 서버 실행
+app.listen(port, () => {
+    console.log(`Server is listening on port ${PORT} now.`);
+});
