734a6910662a28d8af71b936505f02ef37dd263b
Juhyuck-Hong <juhyukhong@gmail.com>
2023-04-29 18:20:47 +0900

    Add user login API feature

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const { Users } = require("../models")
 const router = express.Router()
 
 // 회원 가입 API
-// 닉네임, 비밀번호, 비밀번호 확인을 request에서 전달받기
 router.post("/signup", async (req, res) => {
+    // 닉네임, 비밀번호, 비밀번호 확인을 request에서 전달받기
     const { nickname, password, confirm } = req.body
     try {
         // 닉네임은 최소 3자 이상, 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로 구성되었는지 확인
@@ -38,7 +38,7 @@ router.post("/signup", async (req, res) => {
     // 회원 정보 테이블에 등록
     const user = await Users.create({ nickname, password })
     // 가입과 동시에 jwt 발행후 쿠키 전달
-    const token = jwt.sign({ userId: nickname }, "ghdwngur")
+    const token = jwt.sign({ nickname: user.nickname }, "ghdwngur")
     res.cookie("authorization", `Bearer ${token}`)
     // 회원가입 완료 메세지와 201 상태 코드 전달
     return res.status(201).json({ "message": "회원 가입에 성공하였습니다." })
@@ -46,11 +46,25 @@ router.post("/signup", async (req, res) => {
 })
 
 // 로그인 API
-// 닉네임, 비밀번호를 request에서 전달받기
-// 로그인 버튼을 누른 경우 닉네임과 비밀번호가 데이터베이스에 등록됐는지 확인한 뒤, 
-// 하나라도 맞지 않는 정보가 있다면 "닉네임 또는 패스워드를 확인해주세요."라는 에러 메세지를 response에 포함하기
-// 로그인 성공 시, 로그인에 성공한 유저의 정보를 JWT를 활용하여 클라이언트에게 Cookie로 전달하기
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
+    // 닉네임, 비밀번호를 request에서 전달받기
+    const { nickname, password } = req.body
+    try {
+        // 로그인 버튼을 누른 경우 닉네임과 비밀번호가 데이터베이스에 등록됐는지 확인한 뒤, 
+        const user = await Users.findOne({ where: { nickname } })
+        // 하나라도 맞지 않는 정보가 있다면 에러메시지와 412 상태 코드 전달
+        if (!user || (password !== user.password)) {
+            return res.status(412).json({ "errorMessage": "닉네임 또는 패스워드를 확인해주세요." })
+        } else {
+            // 로그인 성공 시, 로그인에 성공한 유저의 정보를 JWT를 활용하여 클라이언트에게 Cookie로 전달하기
+            const token = jwt.sign({ nickname: user.nickname }, "ghdwngur")
+            res.cookie("authorization", `Bearer ${token}`)
+            return res.status(200).json({ token })
+        }
+    } catch {
+        // 예외 케이스에서 처리하지 못한 에러
+        return res.status(400).json({ "errorMessage": "로그인에 실패하였습니다." })
+    }
 
 })
 
