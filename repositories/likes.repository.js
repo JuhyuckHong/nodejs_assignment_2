e7a4dcb00d94a516fee9211e588868db59ab2bef
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-08 22:03:36 +0900

    Add error handling in like.repository and service

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -2,45 +2,69 @@ const { Posts, Likes } = require('../models');
 
 class LikesRepository {
     findLike = async (postId, userId) => {
-        return await Likes.findOne({
-            where: {
-                UserId: userId,
-                PostId: postId,
-            },
-        });
+        try {
+            return await Likes.findOne({
+                where: {
+                    UserId: userId,
+                    PostId: postId,
+                },
+            });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.repository > findLike');
+        }
     };
 
     findByPost = async (postId) => {
-        const users = await Likes.findAll({
-            where: { PostId: postId },
-        });
-        return users.length;
+        try {
+            const users = await Likes.findAll({
+                where: { PostId: postId },
+            });
+            return users.length;
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.repository > findByPost');
+        }
     };
 
     createLike = async (postId, userId) => {
-        return await Likes.create({
-            UserId: userId,
-            PostId: postId,
-        });
+        try {
+            return await Likes.create({
+                UserId: userId,
+                PostId: postId,
+            });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.repository > createLike');
+        }
     };
 
     deleteLike = async (postId, userId) => {
-        return await Likes.destroy({
-            where: {
-                PostId: postId,
-                UserId: userId,
-            },
-        });
+        try {
+            return await Likes.destroy({
+                where: {
+                    PostId: postId,
+                    UserId: userId,
+                },
+            });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.repository > deleteLike');
+        }
     };
 
     findAllLikedPosts = async (userId) => {
-        const likedPosts = await Likes.findAll({
-            attributes: ['PostId'],
-            include: [{ model: Posts, attributes: [] }],
-            raw: true,
-            where: { UserId: userId },
-        });
-        return likedPosts;
+        try {
+            return await Likes.findAll({
+                attributes: ['PostId'],
+                include: [{ model: Posts, attributes: [] }],
+                raw: true,
+                where: { UserId: userId },
+            });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.repository > findAllLikedPosts');
+        }
     };
 }
 
--- services/likes.service.js
+++ services/likes.service.js
@@ -6,28 +6,53 @@ class LikesService {
     postsService = new PostsService();
 
     findLikeExist = async (postId, userId) => {
-        return await this.likesRepository.findLike(postId, userId);
+        try {
+            return await this.likesRepository.findLike(postId, userId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.service > findLikeExist');
+        }
     };
 
     createLike = async (postId, userId) => {
-        return await this.likesRepository.createLike(postId, userId);
+        try {
+            return await this.likesRepository.createLike(postId, userId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.service > createLike');
+        }
     };
 
     deleteLike = async (postId, userId) => {
-        return await this.likesRepository.deleteLike(postId, userId);
+        try {
+            return await this.likesRepository.deleteLike(postId, userId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.service > deleteLike');
+        }
     };
 
     findByPost = async (postId) => {
-        return await this.likesRepository.findByPost(postId);
+        try {
+            return await this.likesRepository.findByPost(postId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.service > findByPost');
+        }
     };
 
     findAllLikedPosts = async (userId) => {
-        const likeAllPosts = await this.likesRepository.findAllLikedPosts(
-            userId
-        );
-        const postIds = likeAllPosts.map((val) => val.PostId);
-        const posts = await this.postsService.findSomePosts(postIds);
-        return posts;
+        try {
+            const likeAllPosts = await this.likesRepository.findAllLikedPosts(
+                userId
+            );
+            const postIds = likeAllPosts.map((val) => val.PostId);
+            const posts = await this.postsService.findSomePosts(postIds);
+            return posts;
+        } catch (err) {
+            console.error({ err });
+            throw new Error('likes.service > findAllLikedPosts');
+        }
     };
 }
 
