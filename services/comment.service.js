2cda98758c9d935c9a4792f82cad1805b28906ff
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-08 20:55:52 +0900

    Add error handling in comment.service

--- services/comment.service.js
+++ services/comment.service.js
@@ -4,23 +4,52 @@ class CommentService {
     commentRepository = new CommentRepository();
 
     findOneComment = async (commentId) => {
-        return await this.commentRepository.findOneComment(commentId);
+        try {
+            return await this.commentRepository.findOneComment(commentId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('comment.service > findOneComment');
+        }
     };
 
     findAllComments = async (postId) => {
-        return await this.commentRepository.findAllComments(postId);
+        try {
+            return await this.commentRepository.findAllComments(postId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('comment.service > findAllComments');
+        }
     };
 
     createComment = async (postId, userId, comment) => {
-        return this.commentRepository.createComment(postId, userId, comment);
+        try {
+            return this.commentRepository.createComment(
+                postId,
+                userId,
+                comment
+            );
+        } catch (err) {
+            console.error({ err });
+            throw new Error('comment.service > createComment');
+        }
     };
 
     updateComment = async (commentId, comment) => {
-        return this.commentRepository.updateComment(commentId, comment);
+        try {
+            return this.commentRepository.updateComment(commentId, comment);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('comment.service > updateComment');
+        }
     };
 
     deleteComment = async (commentId) => {
-        return this.commentRepository.deleteComment(commentId);
+        try {
+            return this.commentRepository.deleteComment(commentId);
+        } catch (err) {
+            console.error({ err });
+            throw new Error('comment.service > deleteComment');
+        }
     };
 }
 
