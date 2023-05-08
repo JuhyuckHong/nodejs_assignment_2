75f971208548c5ca58f576224fbae46f8de76dc9
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-08 20:54:28 +0900

    Add error handling in comment.repository

--- repositories/comment.repository.js
+++ repositories/comment.repository.js
@@ -1,55 +1,73 @@
-const { Users, Posts, Comments, Sequelize } = require('../models');
+const { Users, Comments, Sequelize } = require('../models');
 
 class CommentRepository {
     findOneComment = async (commentId) => {
-        const comment = await Comments.findOne({ where: { commentId } });
-        return comment;
+        try {
+            return await Comments.findOne({ where: { commentId } });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('Comment.Repository > findOneComment');
+        }
     };
 
     findAllComments = async (postId) => {
-        const comments = await Comments.findAll({
-            attributes: [
-                'commentId',
-                'UserId',
-                [Sequelize.col('nickname'), 'nickname'],
-                'comment',
-                'createdAt',
-                'updatedAt',
-            ],
-            include: [
-                {
-                    model: Users,
-                    attributes: [],
-                }
-            ],
-            order: [['createdAt', 'DESC']],
-            where: { postId },
-        });
-        return comments;
+        try {
+            const comments = await Comments.findAll({
+                attributes: [
+                    'commentId',
+                    'UserId',
+                    [Sequelize.col('nickname'), 'nickname'],
+                    'comment',
+                    'createdAt',
+                    'updatedAt',
+                ],
+                include: [
+                    {
+                        model: Users,
+                        attributes: [],
+                    },
+                ],
+                order: [['createdAt', 'DESC']],
+                where: { postId },
+            });
+            return comments;
+        } catch (err) {
+            console.error({ err });
+            throw new Error('Comment.Repository > findAllComments');
+        }
     };
 
     createComment = async (postId, userId, comment) => {
-        const createdCommentData = await Comments.create({
-            UserId: userId,
-            PostId: postId,
-            comment,
-        });
-        return createdCommentData;
+        try {
+            return await Comments.create({
+                UserId: userId,
+                PostId: postId,
+                comment,
+            });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('Comment.Repository > createComment');
+        }
     };
 
     updateComment = async (commentId, comment) => {
-        const updatedCommentData = await Comments.update(
-            { comment },
-            { where: { commentId } }
-        );
-        return updatedCommentData;
+        try {
+            return await Comments.update({ comment }, { where: { commentId } });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('Comment.Repository > updateComment');
+        }
     };
 
     deleteComment = async (commentId) => {
-        const deleteCommentResult = await Comments.destroy({
-            where: { commentId },
-        });
-        return deleteCommentResult;
+        try {
+            return await Comments.destroy({
+                where: { commentId },
+            });
+        } catch (err) {
+            console.error({ err });
+            throw new Error('Comment.Repository > deleteComment');
+        }
     };
 }
 
